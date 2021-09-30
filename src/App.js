115f9d516d7dd44c64c0d77d573f6e373bce4f08
Shaker Kattoula <smkattoula@gmail.com>
2021-09-30 10:39:55 -0700

    initial commit, create navbar, about, mission and signin components, embed bootstrap, clean up file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Mission from "./components/Mission";
+import Signin from "./components/Signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <About />
+      <Mission />
+      <Signin />
     </div>
   );
 }
